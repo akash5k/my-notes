681d521c18f487b35a0f743b0bf06dffec9b1cef
akash5k <akashparida185@gmai.comk>
2023-11-12 17:59:05 +0530

    initial upload

---
+++ app/src/main/java/com/myNotes/androidsemproject/SettingsFragment.java
@@ -0,0 +1,36 @@
+package com.myNotes.androidsemproject;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.jpkhawam.nabu.R;
+
+import java.util.Objects;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+    // Immediately Reloads SettingsActivity After Preference Change For Immediate Change Response
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        requireActivity().recreate();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Objects.requireNonNull(getPreferenceManager().getSharedPreferences()).registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onPause() {
+        Objects.requireNonNull(getPreferenceManager().getSharedPreferences()).unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+}
